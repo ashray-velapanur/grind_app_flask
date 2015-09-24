70edaa2bdbfa58bee3eb72b6c10276e604ad35a6
Ashray <ashray.velapanur@gmail.com>
2015-09-24 14:34:43 +0530

    creating rooms

--- db/space.py
+++ db/space.py
@@ -1,4 +1,4 @@
-from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.fields import HashKey, RangeKey
 from boto.dynamodb2.items   import Item
 from boto.dynamodb2.table   import Table
 from boto.dynamodb2.exceptions import JSONResponseError
@@ -8,7 +8,7 @@ class SpaceController:
 	def __init__(self, connection=setup_connection()):
 		self.connection = connection
 	
-	def get_or_create_table(self): #fix this!
+	def get_or_create_space(self): #fix this!
 		try:
 			status = self.connection.describe_table("Spaces")['Table']['TableStatus']
 			if status == "ACTIVE":
@@ -16,8 +16,22 @@ class SpaceController:
 		except JSONResponseError:
 			return Table.create('Spaces', schema=[HashKey('space_id')], connection=self.connection);
 		
+	def get_or_create_room(self): #fix this!
+		try:
+			status = self.connection.describe_table("Rooms")['Table']['TableStatus']
+			if status == "ACTIVE":
+				return Table("Rooms", connection=self.connection)
+		except JSONResponseError:
+			return Table.create('Rooms', schema=[HashKey('space_id'), RangeKey('room_id')], connection=self.connection);
+
 	def create_space(self, name):
-		table = self.get_or_create_table()
+		table = self.get_or_create_space()
 		item = Item(table, data={'space_id': name.lower().replace(' ', '_'), 'name': name})
+		item.save()
+
+	def create_room(self, space, name, price):
+		table = self.get_or_create_room()
+		space_id = space['space_id']
+		item = Item(table, data={'space_id': space_id, 'room_id': name.lower().replace(' ', '_'), 'name': name, 'price': price})
 		item.save()
 
