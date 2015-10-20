9c231cbf5d2b10ccbcec35064746ee2c0b725c71
Ashray <ashray.velapanur@gmail.com>
2015-10-20 14:45:45 +0530

    eventbrite config

--- config.py
+++ config.py
@@ -26,3 +26,9 @@ SUPERSAAS = {
 	'username': 'beagles',
 	'password': 'beagles666'
 }
+
+EVENTBRITE = {
+	'auth_url': 'https://www.eventbrite.com/oauth/authorize',
+	'token_url': 'https://www.eventbrite.com/oauth/token',
+	'token': '5N7I4HHI2OYIK3OJXGSX'
+}
